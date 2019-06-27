7c402bef7907b0ca60bbbb149e2b071bf86d5035
Jason Femino <jfemino@godaddy.com>
2019-06-27 11:14:59 -0700

    Updated project to support deployment to pypi.org:
    - Updated setup.py to support additional features:
      - Added more comprehensive clean command
      - Modified setup to embed README.md as pypi.org project description
    - Created a 'domainconnect' pypi.org user account for all public Domain Connect Python artifacts
    - Created script to clean/build/test/deploy build artifact to public pypi.org servers
    - Made fixes to both Markdown and AsciiDoc README files

--- setup.py
+++ setup.py
@@ -3,21 +3,72 @@
 
 from __future__ import unicode_literals
 
+import io
+import os
+
+import distutils.cmd
 from setuptools import setup, find_packages
 
+# ----------------------------------------
+# Description
+# ----------------------------------------
+DESCRIPTION = 'Domain Connect Zone Utility'
+LONG_DESCRIPTION = DESCRIPTION
+LONG_DESCRIPTION_CONTENT_TYPE = ''
+# Place contents of README into 'LONG_DESCRIPTION' for display on pypi.org project page.
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        LONG_DESCRIPTION = '\n' + f.read()
+    LONG_DESCRIPTION_CONTENT_TYPE = 'text/plain',
+except FileNotFoundError:
+    pass
 
-setup(name='domainconnectzone',
-      version='2.1',
-      description='Domain Connect Zone Utility',
-      author='domainconnect.org',
-      url='https://github.com/Domain-Connect/domainconnectzone',
-      packages=find_packages(),
-      install_requires=[
-          'cryptography>=1.8',
-          'dnspython>=1.16',
-          'IPy>=1.0'
-          ],
-      classifiers=[
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 3"
-      ])
+
+# ----------------------------------------
+# Clean
+# ----------------------------------------
+class CleanCommand(distutils.cmd.Command):
+    description = "A better cleaner."
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        cmd_list = dict(
+            build="rm -rf .eggs .pytest_cache dist *.egg-info",
+            DS_Store="find ./ -name .DS_Store -delete;",
+            pyc="find ./ -name '*.pyc' -delete;",
+            empty_dirs="find ./ -type d -empty -delete;"
+        )
+
+        for key, cmd in cmd_list.items():
+            os.system(cmd)
+
+
+setup(
+    name='domainconnectzone',
+    version='2.1',
+    description=DESCRIPTION,
+    author='domainconnect.org',
+    url='https://github.com/Domain-Connect/domainconnectzone',
+    long_description=LONG_DESCRIPTION,
+    # long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    packages=find_packages(),
+    install_requires=[
+        'cryptography>=1.8',
+        'dnspython>=1.16',
+        'IPy>=1.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ],
+    cmdclass={
+      'clean': CleanCommand,
+    },
+)
