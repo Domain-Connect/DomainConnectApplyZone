237304cda665981563638da6ce7c52ce29fa6179
Dave Stys <davestys@gmail.com>
2020-12-18 15:09:49 -0500

    Allow CNAME records to have leading underscores. Example: _domainkey is needed for DKIM CNAME records.

--- domainconnectzone/validate.py
+++ domainconnectzone/validate.py
@@ -73,6 +73,8 @@ def is_valid_host_cname(input):
     if len(input) > 253:
         return False
 
+    input = input.lstrip('_') #allow a leading underscore.
+
     if input[-1] == ".":
         input = input[:-1] # strip exactly one dot from the right, if present
     allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
