3bf12dd41458627b3b8f28395c506924518f4642
Pawel K <2068236-paulokow@users.noreply.gitlab.com>
2022-07-03 20:05:26 +0200

    Merge source from PyPI and bump version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class CleanCommand(Command):
 
 setup(
     name='domainconnectzone',
-    version='3.1.0',
+    version='3.3.0',
     description=DESCRIPTION,
     author='domainconnect.org',
     url='https://github.com/Domain-Connect/domainconnectzone',
