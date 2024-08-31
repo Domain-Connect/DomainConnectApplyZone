e10f2fac4c1f7e8033a29d8a064b71f992de33a4
Pawel Kowalik <pawel-kow@users.noreply.github.com>
2024-09-01 01:17:15 +0200

    RELEASE: 4.0.0 documented and released

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class CleanCommand(Command):
 
 setup(
     name='domainconnectzone',
-    version='3.3.0',
+    version='4.0.0',
     description=DESCRIPTION,
     author='domainconnect.org',
     url='https://github.com/Domain-Connect/domainconnectzone',
