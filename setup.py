87956b0b2e552bd763f424398c7d6bdfdf32bf5e
Pawel Kowalik <pawel-kow@users.noreply.github.com>
2026-04-01 11:22:55 +0200

    update dev version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class CleanCommand(Command):
 
 setup(
     name='domainconnectzone',
-    version='4.7.2',
+    version='4.7.3-dev1',
     description=DESCRIPTION,
     author='domainconnect.org',
     url='https://github.com/Domain-Connect/domainconnectzone',
